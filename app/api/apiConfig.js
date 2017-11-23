70ac33bb8a14bb8bd76551404d7e209754c4b05a
Dark Sun <dacom.dark.sun@gmail.com>
2017-11-24 04:51:12 +0500

    nodes

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -20,20 +20,10 @@ export const settingsAPIs = {
     DEFAULT_WS_NODE: "wss://fake.automatic-selection.com",
     WS_NODE_LIST: [
         {url: "wss://fake.automatic-selection.com", location: {translate: "settings.api_closest"}},
-        {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
-        {url: "wss://bitshares.openledger.info/ws", location: "Nuremberg, Germany"},
-        {url: "wss://eu.openledger.info/ws", location: "Berlin, Germany"},
-        {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
-        {url: "wss://bts.ai.la/ws", location: "Hong Kong"},
-        {url: "wss://bitshares.apasia.tech/ws", location: "Bangkok, Thailand"},
-        {url: "wss://bitshares.dacplay.org/ws", location:  "Hangzhou, China"},
-        {url: "wss://bitshares-api.wancloud.io/ws", location:  "China"},
-        {url: "wss://openledger.hk/ws", location: "Hong Kong"},
-        {url: "wss://dexnode.net/ws", location: "Dallas, USA"},
-        {url: "wss://bitshares.crypto.fans/ws", location: "Munich, Germany"},
-        {url: "wss://node.testnet.bitshares.eu", location: "Public Testnet Server (Frankfurt, Germany)"}
+        {url: "wss://testnet.travelchain.io/ws", location: "unknown"},
+       
     ],
-    DEFAULT_FAUCET: "https://bitshares.openledger.info",
-    TESTNET_FAUCET: "https://faucet.testnet.bitshares.eu",
+    DEFAULT_FAUCET: "https://testnet.travelchain.io/faucet",
+    TESTNET_FAUCET: "https://testnet.travelchain.io/faucet",
     RPC_URL: "https://openledger.info/api/"
 };
