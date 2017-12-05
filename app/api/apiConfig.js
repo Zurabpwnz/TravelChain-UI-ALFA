1d5fd55aea20dbef24548c8eb88ef7334b630e17
avral <qweasd_96@mail.ru>
2017-12-05 19:36:17 +0300

    chainge default faucet

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -21,8 +21,7 @@ export const settingsAPIs = {
     WS_NODE_LIST: [
         {url: "wss://testnet.travelchain.io/ws", location: "Public Testnet Server"},
     ],
-    DEFAULT_FAUCET: "http://localhost:8000/api/faucet/",
-    // TESTNET_FAUCET: "https://testnet.travelchain.io/faucet",
-    TESTNET_FAUCET: "http://localhost:8000/api/faucet/",
+    DEFAULT_FAUCET: "https://travelchain.io/api/faucet/",
+    TESTNET_FAUCET: "https://testnet.travelchain.io/api/faucet/",
     RPC_URL: "https://openledger.info/api/"
 };
